24ccf813aa750481dc6efc6a338cfce5aee20919
Tyler Blakeman <tylerblakeman@gmail.com>
2020-03-27 23:17:32 -0500

    array of questions created, working properly and console logging

---
+++ assets/javascript/app.js
@@ -0,0 +1,47 @@
+//global gameplay variables
+var questionTime
+var quizTime
+var questionsAndAnswers = [{
+    question: "Question One?",
+    correctAnswer: "Correct Answer",
+    answers: ["Incorrect 1", "Incorrect 2", "Incorrect 3"]
+},
+{
+    question: "Question Two?",
+    correctAnswer: "Correct Answer",
+    answers: ["Incorrect 1", "Incorrect 2", "Incorrect 3", "Correct Answer"]
+},
+{
+    question: "Question Three?",
+    correctAnswer: "Correct Answer",
+    answers: ["Incorrect 1", "Incorrect 2", "Incorrect 3", "Correct Answer"]
+},
+{
+    question: "Question Four?",
+    correctAnswer: "Correct Answer",
+    answers: ["Incorrect 1", "Incorrect 2", "Incorrect 3", "Correct Answer"]
+},
+{
+    question: "Question Five?",
+    correctAnswer: "Correct Answer",
+    answers: ["Incorrect 1", "Incorrect 2", "Incorrect 3", "Correct Answer"]
+},
+{
+    question: "Question Six?",
+    correctAnswer: "Correct Answer",
+    answers: ["Incorrect 1", "Incorrect 2", "Incorrect 3", "Correct Answer"]
+},
+{
+    question: "Question Seven?",
+    correctAnswer: "Correct Answer",
+    answers: ["Incorrect 1", "Incorrect 2", "Incorrect 3", "Correct Answer"]
+}]
+
+console.log(questionsAndAnswers[0]);
+console.log(questionsAndAnswers[1]);
+console.log(questionsAndAnswers[2]);
+console.log(questionsAndAnswers[3]);
+console.log(questionsAndAnswers[4]);
+console.log(questionsAndAnswers[5]);
+console.log(questionsAndAnswers[6]);
+
