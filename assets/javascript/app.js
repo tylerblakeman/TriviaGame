3ce90c682b6a90d13682fd6b4444367ffd2ccbb5
Tyler Blakeman <tylerblakeman@gmail.com>
2020-03-30 18:52:30 -0500

    function to write buttons to the page added - timer function works to time out and display next question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,7 @@
 //global gameplay variables
 var questionTime;
 var timerInterval;
-var numberOfQuestions = 6;
-var questionsRemaining;
+var numberOfQuestions = 2;
 var randomQuestion;
 var questionsToAsk = [];
 var questionsRight = 0;
@@ -10,7 +9,7 @@ var questionsWrong = 0;
 var questionsAndAnswers = [{
     question: "Question One?",
     correctAnswer: "Correct Answer",
-    answers: ["Incorrect 1", "Incorrect 2", "Incorrect 3"]
+    answers: ["Incorrect 1", "Incorrect 2", "Incorrect 3", "Correct Answer"]
 },
 {
     question: "Question Two?",
@@ -124,41 +123,53 @@ var questionsAndAnswers = [{
 }];
 
 
+var questiondiv = `<div class = "question">`;
+var closediv = "<\div>";
+var button = "<button>";
+
 //html variables
 var displayArea = document.getElementById("contentarea");
 var startButton = document.getElementById("start");
-var timeHtml = document.getElementById("timer")
+var timeHtml = document.getElementById("timer");
+var timeRemaining = document.getElementById("timeremaining")
 
-
- 
-//select a random question - picking from array length, allowing for an assortment of questions
+ //select a random question - picking from array length, allowing for an assortment of questions
 function randomQuestions(){
     for (let i = 0; i < numberOfQuestions; i++) {
     randomQuestion = Math.floor(Math.random() * (questionsAndAnswers.length));
     questionsToAsk.push(questionsAndAnswers[randomQuestion]);
     questionsAndAnswers.splice(randomQuestion, 1);
-    //allows us to ask all of the questions 
-    questionsRemaining = numberOfQuestions;
 }
 }
 
 //start button click starts the game and calls for the first question - this is displayed at the start of the game and the end of the game
 startButton.onclick = function(){
+    resetGame();
     randomQuestions();
     displayArea.innerHTML = "";
     displayQuestion()
-    numberOfQuestions--
     console.log(numberOfQuestions)
-    displayQuestion();
 }
+
+//start button click starts the game and calls for the first question - this is displayed at the start of the game and the end of the game
+
 
 //writing questions to html - currently testing that it can display for 6 seconds.
 function displayQuestion(){
-    displayArea.innerHTML = "blah blah blah blah blah blah";    
+    var x = questionsRight + questionsWrong;
+    var questionPush = questiondiv + questionsToAsk[x].question + closediv;
+    console.log(questionPush)
+    var answerPush ="";
+    for (let i = 0; i < questionsToAsk[x].answers.length; i++) {
+        answerPush = answerPush + button + questionsToAsk[x].answers[i];
+        }
+    console.log(answerPush)
+    displayArea.innerHTML = questionPush + answerPush; 
+    timer()
 }
 
 
-//countdown timer reset and call timer - pulled from https://github.com/tylerblakeman/Playing-With-Timers - my collaboration with Zach Hassler
+//countdown timer reset and call timer - pulled from https://github.com/tylerblakeman/Playing-With-Timers - my collaboration with Zach Hassler for this project - I struggled with timers and worked through this with him
 function timer(){
     questionTime = 5;
     timerInterval = setInterval(countdown, 1000)
@@ -169,9 +180,49 @@ function timer(){
 function countdown() {
     if (questionTime == 0) {
     clearInterval(timerInterval)
-    return console.log("time's up!...TYLER!");
+    timeRemaining.innerHTML = "Your time is up!";
+    questionsWrong++;
+    endOfGame();
     } else {
     console.log(questionTime)
     questionTime--;
+    timeRemaining.innerHTML = questionTime + " seconds";
     }
+}
+
+//check answer function
+function checkAnswer(){
+    if (userSelection = questionsToAsk[x].correctAnswer){
+        questionsRight++;
+        endOfGame()
+    }
+    else {
+        questionsWrong++
+        endOfGame()
+    }
+}
+
+//game over?
+function endOfGame(){
+    if (numberOfQuestions == questionsRight + questionsWrong) {
+        console.log("End of Game!");
+        displayArea.innerHTML = questiondiv + "would you like to play again?" + closediv + `<button id="start2">Start Game</button>`;
+        var startButton2 = document.getElementById("start2")
+        startButton2.onclick = function(){
+            displayArea.innerHTML = "";
+            resetGame();
+            randomQuestions();
+            displayQuestion()
+            console.log(numberOfQuestions)
+        }
+        }
+    else {
+        displayQuestion();
+    }
+}
+
+function resetGame(){
+    questionsToAsk = [];
+    questionsRight = 0;
+    questionsWrong = 0;
 }
